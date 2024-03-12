60095b1087ce63bbf1c980252d7627ffd783d1e2
Charlie929 <787508720@qq.com>
2024-03-13 00:58:48 +0800

    temp ch1

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,12 +1,34 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
+#[macro_use]
+mod console;
 mod lang_items;
+mod sbi;
+mod uart;
 
 // 在 Rust 代码中直接插入汇编指令
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
-fn main() {
-    // println!("Hello, world!");
+// avoid confusing names
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
+
+// initialize .bss section
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+
